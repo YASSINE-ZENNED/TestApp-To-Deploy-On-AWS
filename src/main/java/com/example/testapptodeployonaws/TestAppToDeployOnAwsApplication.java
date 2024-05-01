3f03480d352e069bf77e9e5749851a2b820e17b4
YASSINE ZENNED <yassine.zenned.123@gmail.com>
2024-05-01 13:54:18 +0100

    ship to github

---
+++ src/main/java/com/example/testapptodeployonaws/TestAppToDeployOnAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testapptodeployonaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAppToDeployOnAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAppToDeployOnAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testapptodeployonaws/TestAppToDeployOnAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testapptodeployonaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAppToDeployOnAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
